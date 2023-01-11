664588a7eec95a6b380c4bfa4c450c915b710417
ParshwaGundecha <88487547+ParshwaGundecha@users.noreply.github.com>
2023-01-11 20:08:36 +0530

    Add files via upload

---
+++ Video-Album.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+
+path = "Images"
+
+images = []
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg']:
+        file_name = path+"/"+file
+
+        images.append(file_name)
+
+print(images[0])
+count = len(images)
+
+frame = cv2.imread(images[0])
+height, width, channel = frame.shape
+
+size = (width, height)
+
+out = cv2.VideoWriter(
+    'project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 0.8, size)
+
+for i in range(0, count):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
+print('Done')
